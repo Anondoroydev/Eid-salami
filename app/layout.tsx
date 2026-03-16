2cb0c13763e87c1460fd41edcab1fe5f77b27694
Ajoy Sarkar <155249242+Anondoroydev@users.noreply.github.com>
2026-03-17 00:28:20 +0600

    Project done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,25 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
+const geist = Geist({ subsets: ["latin"] });
+const geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL || 'https://localhost:3000'),
-  title: 'ঈদ মোবারক | Eid Mubarak',
-  description: 'ঈদের শুভেচ্ছা জানান প্রিয়জনদের - বিকাশ, নগদ, রকেট দিয়ে ঈদি পাঠান',
-  generator: 'Next.js',
-
-}
+  title: "ঈদ মোবারক | Eid Mubarak",
+  description:
+    "ঈদের শুভেচ্ছা জানান প্রিয়জনদের - বিকাশ, নগদ, রকেট দিয়ে ঈদি পাঠান",
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className="font-sans antialiased">
+      <body className={`${geist.className} ${geistMono.className} antialiased`}>
         {children}
         <Analytics />
       </body>
